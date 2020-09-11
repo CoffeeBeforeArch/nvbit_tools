3d4eefd98b62aa2fb549a098b958a88d97aa04ef
Nick <CoffeeBeforeArch@gmail.com>
2020-09-11 01:32:13 -0500

    add API print tool

---
+++ tools/print_api_calls/print_api_calls.cu
@@ -0,0 +1,129 @@
+/* Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <assert.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <unordered_set>
+
+/* every tool needs to include this once */
+#include "nvbit_tool.h"
+
+/* nvbit interface file */
+#include "nvbit.h"
+
+/* nvbit utility functions */
+#include "utils/utils.h"
+
+/* kernel id counter, maintained in system memory */
+uint32_t kernel_id = 0;
+
+/* total instruction counter, maintained in system memory, incremented by
+ * "counter" every time a kernel completes  */
+uint64_t tot_app_instrs = 0;
+
+/* kernel instruction counter, updated by the GPU */
+__managed__ uint64_t counter = 0;
+
+/* global control variables for this tool */
+uint32_t instr_begin_interval = 0;
+uint32_t instr_end_interval = UINT32_MAX;
+uint32_t start_grid_num = 0;
+uint32_t end_grid_num = UINT32_MAX;
+int verbose = 0;
+int count_warp_level = 1;
+int exclude_pred_off = 0;
+int active_from_start = 1;
+bool mangled = false;
+
+/* used to select region of insterest when active from start is off */
+bool active_region = true;
+
+/* a pthread mutex, used to prevent multiple kernels to run concurrently and
+ * therefore to "corrupt" the counter variable */
+pthread_mutex_t mutex;
+
+/* nvbit_at_init() is executed as soon as the nvbit tool is loaded. We typically
+ * do initializations in this call. In this case for instance we get some
+ * environment variables values which we use as input arguments to the tool */
+void nvbit_at_init() {
+    /* just make sure all managed variables are allocated on GPU */
+    setenv("CUDA_MANAGED_FORCE_DEVICE_ALLOC", "1", 1);
+
+    /* we get some environment variables that are going to be use to selectively
+     * instrument (within a interval of kernel indexes and instructions). By
+     * default we instrument everything. */
+    GET_VAR_INT(
+        instr_begin_interval, "INSTR_BEGIN", 0,
+        "Beginning of the instruction interval where to apply instrumentation");
+    GET_VAR_INT(
+        instr_end_interval, "INSTR_END", UINT32_MAX,
+        "End of the instruction interval where to apply instrumentation");
+    GET_VAR_INT(start_grid_num, "START_GRID_NUM", 0,
+                "Beginning of the kernel gird launch interval where to apply "
+                "instrumentation");
+    GET_VAR_INT(
+        end_grid_num, "END_GRID_NUM", UINT32_MAX,
+        "End of the kernel launch interval where to apply instrumentation");
+    GET_VAR_INT(count_warp_level, "COUNT_WARP_LEVEL", 1,
+                "Count warp level or thread level instructions");
+    GET_VAR_INT(exclude_pred_off, "EXCLUDE_PRED_OFF", 0,
+                "Exclude predicated off instruction from count");
+    GET_VAR_INT(
+        active_from_start, "ACTIVE_FROM_START", 1,
+        "Start instruction counting from start or wait for cuProfilerStart "
+        "and cuProfilerStop");
+    GET_VAR_INT(mangled, "MANGLED_NAMES", 1,
+                "Print kernel names mangled or not");
+
+    GET_VAR_INT(verbose, "TOOL_VERBOSE", 0, "Enable verbosity inside the tool");
+    if (active_from_start == 0) {
+        active_region = false;
+    }
+
+    std::string pad(100, '-');
+    printf("%s\n", pad.c_str());
+}
+
+/* Set used to avoid re-instrumenting the same functions multiple times */
+std::unordered_set<CUfunction> already_instrumented;
+
+/* This call-back is triggered every time a CUDA driver call is encountered.
+ * Here we can look for a particular CUDA driver call by checking at the
+ * call back ids  which are defined in tools_cuda_api_meta.h.
+ * This call back is triggered bith at entry and at exit of each CUDA driver
+ * call, is_exit=0 is entry, is_exit=1 is exit.
+ * */
+void nvbit_at_cuda_event(CUcontext ctx, int is_exit, nvbit_api_cuda_t cbid,
+                         const char *name, void *params, CUresult *pStatus) {
+  printf("%s\n", name);
+}
+
+void nvbit_at_term() {
+    printf("END OF APPLICATION\n");
+}
